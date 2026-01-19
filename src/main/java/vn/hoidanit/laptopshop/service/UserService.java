76785b44ebd173f273f30f1eb7aad59e888edb28
Huy Ngo <baphuy48@gmail.com>
2026-01-20 03:03:14 +0700

    Upload code how to send data with HTML Form

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -7,4 +7,11 @@ public class UserService {
     public String handleHello() {
         return "Hello form service";
     }
+
+    public class UserPageService {
+        public String handleUserPage() {
+            return "User form service";
+        }
+
+    }
 }
